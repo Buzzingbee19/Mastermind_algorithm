f5b87f2a498ed53d15463808ef56d6967ce0e1d6
Buzzingbee19 <pbuzza5@gmail.com>
2017-01-23 16:04:41 -0500

    Update to main file

--- main.cpp
+++ main.cpp
@@ -1,34 +1,41 @@
-//
- +//  main.cpp
- +//  DraftMasterMind
- +//
- +//  Created by Adam Bechtold on 1/22/17.
- +//  Copyright © 2017 Adam Bechtold. All rights reserved.
- +//
- +
- +//QUESTIONS
- +// 1 -> SEE code.cpp
- +// 2 -> SEE code.h
- +// 3) naming conventions for classes
- +
- +#include <iostream>
-  #include "code.h"
- -#include "d_random.h"
+ //  main.cpp
+ //  DraftMasterMind
+ //
+ //  Created by Adam Bechtold on 1/22/17.
+ //  Copyright © 2017 Adam Bechtold. All rights reserved.
+ //
+ #include <iostream>
+ #include "code.h"
+ #include "d_random.h"
+using namespace std
 
- -void main(){
- -	int range, digits;
- -	cout<<"Welcome to Mastermind, the scintillating game to test your reasoning skills! \n How many digits would like to have in your code?";
- -	cin>>digits;
- -	cout<<"What would you like your range of numbers to be?";
- -	cin>>range;
- -	code mastercode(digits, range);
- -	vector<int> guess(digits);
- -	for (int i=0; i<digits;i++){
- -		cout<<"Please input your guess for value "<<i<<"\n";
- -		cin>>guess(i);
- -	}
- -	int numcorrect = mastercode.checkCorrect(guess);
- -	int numincorrect = mastercode.checkIncorrect(guess);
- -	cout<<"There were "<<numcorrect<<" correct numbers in their correct location, and ""<<numincorrect<<"" numbers in their incorrect location. \n"";
+ void main(){
+ int range, digits;  //initialize the variables taken in from the users to construct our code class
+ cout<<"Welcome to Mastermind, the scintillating game to test your reasoning skills! \n How many digits would like to have in your code?";
+ cin>>digits;
+ cout<<"What would you like your range of numbers to be?";
+ cin>>range;
+ code mastercode(digits, range);
+ vector<int> guess(digits);
+ for (int i=0; i<digits;i++){
+  	cout<<"Please input your guess for value "<<i<<"\n";
+  	cin>>guess(i);
+ }
+ int numcorrect = mastercode.checkCorrect(guess);
+ int numincorrect = mastercode.checkIncorrect(guess);
+ cout<<"There were "<<numcorrect<<" correct numbers in their correct location, and "<<numincorrect<<" numbers in their incorrect location. \n";
+/*
+long guessOneArray[] = {5, 0, 3, 2, 6};
+long guessTwoArray[] = {2, 1, 2, 2, 2};
+long guessThreeArray[] = {1, 3, 3, 4, 5};
+vector<long> guessOne(guessOneArray, guessOneArray + 5);
+vector<long> guessTwo(guessTwoArray, guessTwoArray + 5);
+vector<long> guessThree(guessThreeArray, guessThreeArray + 5);
 
- -};
+
+//check correctness
+cout << "[" << myCode.checkCorrect(guessOne) << "," << myCode.checkIncorrect(guessOne) << "]" << endl;
+cout << "[" << myCode.checkCorrect(guessTwo) << "," << myCode.checkIncorrect(guessTwo) << "]" << endl;
+cout << "[" << myCode.checkCorrect(guessThree) << "," << myCode.checkIncorrect(guessThree) << "]" << endl;
+*/
+  };
