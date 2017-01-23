22a266f10045466f39a8e177742a94338327e770
adambechtold <adamcbechtold@gmail.com>
2017-01-23 16:13:10 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,34 @@
 using namespace std
 
  void main(){
+ 
+ //start
+ Code myCode = Code(5, 10);
+    
+ //take a guess
+ long guessOneArray[] = {5, 0, 3, 2, 6};
+ long guessTwoArray[] = {2, 1, 2, 2, 2};
+ long guessThreeArray[] = {1, 3, 3, 4, 5};
+ vector<long> guessOne(guessOneArray, guessOneArray + 5);
+ vector<long> guessTwo(guessTwoArray, guessTwoArray + 5);
+ vector<long> guessThree(guessThreeArray, guessThreeArray + 5);
+    
+    
+ //check correctness
+ cout << "\nresponse format: [correct digit in correct posistion, correct digit in wrong position]\n" << endl;
+ cout << "guess:       (5, 0, 3, 2, 6) -> ";
+ cout << "[" << myCode.checkCorrect(guessOne) << "," << myCode.checkIncorrect(guessOne) << "]" << endl;
+ cout << "guess:       (2, 1, 2, 2, 2) -> ";
+ cout << "[" << myCode.checkCorrect(guessTwo) << "," << myCode.checkIncorrect(guessTwo) << "]" << endl;
+ cout << "guess:       (1, 3, 3, 4, 5) -> ";
+ cout << "[" << myCode.checkCorrect(guessThree) << "," << myCode.checkIncorrect(guessThree) << "]" << endl;
+    
+ cout << endl;
+
+ 
+ 
+ 
+ //Playing a new game
  int range, digits;  //initialize the variables taken in from the users to construct our code class
  cout<<"Welcome to Mastermind, the scintillating game to test your reasoning skills! \n How many digits would like to have in your code?";
  cin>>digits;
@@ -38,4 +66,4 @@ cout << "[" << myCode.checkCorrect(guessOne) << "," << myCode.checkIncorrect(gue
 cout << "[" << myCode.checkCorrect(guessTwo) << "," << myCode.checkIncorrect(guessTwo) << "]" << endl;
 cout << "[" << myCode.checkCorrect(guessThree) << "," << myCode.checkIncorrect(guessThree) << "]" << endl;
 */
-  };
+};
